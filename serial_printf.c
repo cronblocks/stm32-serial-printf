2d6070756d9435c0c1b37f5ab3e399d36151d6a5
usama <mahmoodify@gmail.com>
2022-06-11 10:54:11 +0500

    Adding binary conversion support

--- serial_printf.c
+++ serial_printf.c
@@ -30,14 +30,55 @@ void serial_printf(UART_HandleTypeDef* uart_handle_ptr, const char* format_str, 
 	int final_str_index = 0;
 	char next_char;
 
+	int __temp_index;
+	char __temp_char;
+	unsigned int __temp_uint;
+
 	for (int format_str_index = 0; format_str_index < strlen(format_str); format_str_index++) {
 
 		next_char = format_str[format_str_index + 1];
 
 		if (format_str[format_str_index] != '%') {
+
 			final_str[final_str_index++] = format_str[format_str_index];
+
 		} else {
+
 			switch (next_char) {
+
+			case 'b':
+			case 'B':
+				__temp_uint = va_arg(va_args, unsigned int);
+				__temp_index = final_str_index;
+
+				while(__temp_uint) {
+					final_str[__temp_index++] = 48 + (__temp_uint % 2);
+					final_str[__temp_index] = 0;
+					__temp_uint /= 2;
+				}
+
+				if (__temp_index == final_str_index) {
+
+					// Value is zero
+					sprintf(&final_str[final_str_index], "%d", 0);
+
+				} else {
+
+					// Reverse the order
+					__temp_index--;
+					while (__temp_index > final_str_index) {
+						__temp_char = final_str[final_str_index];
+						final_str[final_str_index] = final_str[__temp_index];
+						final_str[__temp_index] = __temp_char;
+
+						final_str_index++;
+						__temp_index--;
+					}
+				}
+
+				final_str_index = strlen(final_str);
+				format_str_index++;
+				break;
 
 			case 'd':
 			case 'D':
