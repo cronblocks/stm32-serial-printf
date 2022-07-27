62fdcd719f0c12829077d48fa4acfe1caf28379e
usama <mahmoodify@gmail.com>
2022-07-28 02:27:13 +0500

    Update serial_console.c

--- serial_console.c
+++ serial_console.c
@@ -5,7 +5,7 @@
  *
  *****************************************************************************************
  * Author:      Usama
- * Created on:  Jun 10, 2022
+ * Created on:  July 28, 2022
  *****************************************************************************************
  * Changes:
  *
@@ -17,3 +17,40 @@
  *
  *****************************************************************************************/
 
+#include "stm32f7xx_hal.h"
+
+
+UART_HandleTypeDef *console_io_uart_ptr = 0;
+
+
+int __io_putchar(int _char)
+{
+	if (console_io_uart_ptr)
+	{
+		HAL_UART_Transmit(console_io_uart_ptr, (unsigned char *) &_char, 1, HAL_MAX_DELAY);
+		return _char;
+	}
+
+	return 0;
+}
+
+int __io_getchar(void)
+{
+	if (console_io_uart_ptr)
+	{
+		unsigned char _char;
+
+		HAL_UART_Receive(console_io_uart_ptr, &_char, 1, HAL_MAX_DELAY);
+		HAL_UART_Transmit(console_io_uart_ptr, &_char, 1, HAL_MAX_DELAY);
+
+		if (_char == '\r')
+		{
+			unsigned char repl = '\n';
+			HAL_UART_Transmit(console_io_uart_ptr, &repl, 1, HAL_MAX_DELAY);
+		}
+
+		return _char;
+	}
+
+	return 0;
+}
